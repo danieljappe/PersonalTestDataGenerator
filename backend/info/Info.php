89eedc5632e0955c3dd9ac458768207604eb3760
samimsaidi <samimsaidi2660@gmail.com>
2025-10-15 10:44:08 +0200

    Docker

--- backend/info/Info.php
+++ backend/info/Info.php
@@ -2,8 +2,8 @@
 
 class Info
 {
-    public const HOST = 'localhost';
+    public const HOST = 'mariadb';
     public const DB_NAME = 'addresses';
     public const USER = 'root';
-    public const PASSWORD = '';
+    public const PASSWORD = 'password';
 }
